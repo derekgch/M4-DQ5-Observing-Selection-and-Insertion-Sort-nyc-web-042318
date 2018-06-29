f7c861f0de22652d438167842b3bcfb0695113e1
Derek Chen <derekgc@icloud.com>
2018-06-29 12:16:32 -0400

    hopefully no bug

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,28 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let previousIndex;
+
+  for (let index = 0; index < arr.length; index++) {
+    previousIndex = index -1;
+    let swapped = true;
+
+    if(previousIndex > 0){
+      let swapIndex = index;
+      while(swapped){
+        if(arr[previousIndex] > arr[swapIndex]){
+          let temp = arr[previousIndex];
+          arr[previousIndex] = arr[swapIndex];
+          arr[swapIndex] = temp;
+          swapped = true;
+        }else{
+          swapped = false;
+        }
+        swapIndex--;
+        if(swapIndex < 1) swapped = false;
+        previousIndex = swapIndex -1;
+      }
+    }
+  }
+  return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,27 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  const findIndexOfMin = (array, startIndex) =>{
+    let min = array[startIndex];
+    let minIndex = startIndex;
+    let index
+    for ( index = startIndex; index < array.length; index++) {
+      if(min > array[index]){
+        min = array[index];
+        minIndex = index;
+      }      
+    }
+    return minIndex;
+  }
+
+  // console.log(arr)
+
+    for (let index = 0; index < arr.length; index++) {
+      let currentElement = arr[index];
+      let minIndex = findIndexOfMin(arr, index);
+      arr[index] = arr[minIndex];
+      arr[minIndex] = currentElement;
+    }
+  return arr;
 }
